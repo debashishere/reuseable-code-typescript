7e4523e2b694e77677ba7ff516466d5c108a5d5a
debashishere <debashishere007@gmail.com>
2021-08-18 17:02:48 +0530

    adding abstract class & generic

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,20 @@
-import fs from 'fs';
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
+const reader = new CsvFileReader('football.csv');
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n')
-.map((row: string): string[] => {
-  return row.split(',');
-})
+reader.read();
 
-// enum - enumeration
-enum MatchResult {
-  HomwWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+
 
 let manUnitedWins = 0;
 
-for( let match of matches) {
+for( let match of reader.data) {
   if(match[1] === 'Man United' && match[5] == MatchResult.HomwWin){
     manUnitedWins++;
   }else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
     manUnitedWins++;
   }
 }
+
 
 console.log(`Man United Won ${manUnitedWins} games`)
