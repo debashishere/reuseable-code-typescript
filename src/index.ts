4b88682cb4504c48cc28c470a87ac23e3d47754c
debashishere <debashishere007@gmail.com>
2021-08-18 11:59:46 +0530

    naive js approch

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ const matches = fs.readFileSync('football.csv', {
   return row.split(',');
 })
 
+const MatchResult = {
+  HomwWin: 'H',
+  AwayWin: 'A',
+  Draw: 'D'
+}
+
 let manUnitedWins = 0;
 
 for( let match of matches) {
-  if(match[1] === 'Man United' && match[5] == 'H'){
+  if(match[1] === 'Man United' && match[5] == MatchResult.HomwWin){
     manUnitedWins++;
-  }else if (match[2] === 'Man United' && match[5] === 'A'){
+  }else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
     manUnitedWins++;
   }
 }
