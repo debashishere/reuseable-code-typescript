0e25bda0dc0cf5f9e5245651e6abbb0525740b31
debashishere <debashishere007@gmail.com>
2021-08-18 17:36:48 +0530

    adding interface

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,20 +1,34 @@
-import { CsvFileReader } from "./CsvFileReader";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
+import { dateStringToDate } from './utils'
+import { MatchResult } from './MatchResult';
+
 // a cystom type
 type MatchData = [ Date, string, string, number, number, MatchResult, string];
 
-export class MatchReader extends CsvFileReader<MatchData> {
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      // type assersion
-      row[5] as MatchResult ,// 'H', 'A', 'D'
-      row[6]
-    ]
+
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
+
+export class MatchReader {
+  matches : MatchData[] = []
+
+  // reader satisfying DataReader interface
+  constructor(public reader: DataReader){}
+
+  load(): void{
+    this.reader.read();
+    this.matches = this.reader.data.map((row: string[]):MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          // type assersion
+          row[5] as MatchResult ,// 'H', 'A', 'D'
+          row[6]
+        ]
+      })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Create ab Object satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
 
+// Create an instance of MatchReader and pass in something satisfying 
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for( let match of reader.data) {
+for( let match of matchReader.matches) {
   if(match[1] === 'Man United' && match[5] == MatchResult.HomwWin){
     manUnitedWins++;
   }else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
